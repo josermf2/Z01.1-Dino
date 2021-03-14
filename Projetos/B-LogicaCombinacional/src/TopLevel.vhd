268298808ea84d68824e3100baba67b498e46e47
Esther <esthergd@al.insper.edu.br>
2021-03-14 13:24:08 -0300

    funcionando e testado

--- Projetos/B-LogicaCombinacional/src/TopLevel.vhd
+++ Projetos/B-LogicaCombinacional/src/TopLevel.vhd
@@ -38,6 +38,24 @@ architecture rtl of TopLevel is
 -- implementacao
 ---------------
 begin
-          
+	
+	-- LEDR(0) <= SW(0); -- igualando o bit 0 do vetor LEDR ao bit 0 da chave SW
+	-- Fazer a saida LEDR(0) ser igual ao inverso da entrada SW(0)
+	-- LEDR(0) <= SW(0) or SW(1); -- fazendo a saida LEDR(0) ser igual a entrada SW(0) ou SW(1)
+	
+	-- LEDR(0) <= SW(0) or SW(1); -- Fazer com que a saida LEDR(0) seja igual a entrada SW(0) ou SW(1) 
+	-- LEDR(1) <= SW(1); -- E com que a saida LEDR(1) seja igual a chave SW(1)
+	
+	LEDR(0) <= SW(0);
+	LEDR(1) <= SW(1);
+	LEDR(2) <= SW(2);
+	LEDR(3) <= SW(3);
+	LEDR(4) <= SW(4);
+	LEDR(5) <= SW(5);
+	LEDR(6) <= SW(6);
+	LEDR(7) <= SW(7);
+	LEDR(8) <= SW(8);
+	LEDR(9) <= SW(9);
 
 end rtl;
+
