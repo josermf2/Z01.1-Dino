5c0873cd75abf3cdb94439dbb6b80b77cbd22b5c
BrunoSaboya <brunobs6@al.insper.edu.br>
2021-03-17 21:24:55 -0300

    Seven Seguementos Led

--- Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/sevenSeg.vhd
@@ -4,10 +4,24 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity sevenSeg is
 	port (
 			bcd : in  STD_LOGIC_VECTOR(3 downto 0);
-			leds: out STD_LOGIC_VECTOR(6 downto 0));
+			leds: out STD_LOGIC_VECTOR(6 downto 0)
+			);
 end entity;
 
 architecture arch of sevenSeg is
 begin
 
+with bcd select
+
+	leds <= "0000001" when "0000",
+			  "1001111" when "0001",
+			  "0010010" when "0010",
+			  "0000110" when "0011",
+			  "1001100" when "0100",
+			  "0101100" when "0101",
+			  "0100000" when "0110",
+			  "0001111" when "0111",
+			  "0000000" when "1000",
+			  "0001100" when others;
+	
 end architecture;
