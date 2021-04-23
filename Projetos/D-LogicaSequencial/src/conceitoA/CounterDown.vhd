6ec1eb542106d687ae2282a5ed48db880849825a
Esther <esthergd@al.insper.edu.br>
2021-04-23 09:40:42 -0300

    configurando scrum do projeto

--- Projetos/D-LogicaSequencial/src/conceitoA/CounterDown.vhd
+++ Projetos/D-LogicaSequencial/src/conceitoA/CounterDown.vhd
@@ -13,6 +13,17 @@ end entity;
 
 architecture arch of CounterDown is
 
+	signal q0_neg, q1_neg, q2_neg : std_logic;
+ 	signal q0,q1,q2 : std_logic;
+ 
 begin
+ 
+ FFT00: FlipFlopT port map(clock,'1',q0, q0_neg);
+ FFT01: FlipFlopT port map(q0,'1',q1, q1_neg);
+ FFT02: FlipFlopT port map(q1,'1',q2, q2_neg);
+ 
+ q(0)<=q0;
+ q(1)<=q1;
+ q(2)<=q2;
 
 end architecture;
